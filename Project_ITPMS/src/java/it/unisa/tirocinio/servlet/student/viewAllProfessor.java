ce0e215ec021588df9e0c59b868fdeed37315365
valucifer <v.vivone.5389@gmail.com>
2014-12-10 09:50:48 +0100

    commit

---
+++ Project_ITPMS/src/java/it/unisa/tirocinio/servlet/student/viewAllProfessor.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.tirocinio.servlet.student;
+
+import it.unisa.tirocinio.beans.Person;
+import it.unisa.tirocinio.manager.concrete.ConcreteMessageForServlet;
+import it.unisa.tirocinio.manager.concrete.ConcretePerson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Valentino
+ */
+public class viewAllProfessor extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        ConcreteMessageForServlet message = new ConcreteMessageForServlet();
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            ConcretePerson aPerson = ConcretePerson.getInstance();
+            ArrayList<Person> person = aPerson.getAllPeople();
+            
+            ArrayList<Person> allProfessor = new ArrayList<Person>();
+            
+            for(int i = 0; i < person.size(); i++){
+                if(aPerson.isAProfessor(person.get(i).getAccountEmail())){
+                    allProfessor.add(person.get(i));
+                }
+            }
+            
+            if(allProfessor.size() == 0){
+                message.setMessage("status", 0);
+                message.setMessage("description","Non ci sono professori");
+            }else{
+                message.setMessage("status", 1);
+                message.setMessage("Object", allProfessor);
+                for(int i = 0; i < allProfessor.size(); i++)
+                    out.println(allProfessor.get(i).getName());
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Project_ITPMS/src/java/it/unisa/tirocinio/servlet/student/viewTrainingRequestForProfessorServlet.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.tirocinio.servlet.student;
+
+import it.unisa.tirocinio.beans.Person;
+import it.unisa.tirocinio.beans.TrainingRequest;
+import it.unisa.tirocinio.manager.concrete.ConcreteMessageForServlet;
+import it.unisa.tirocinio.manager.concrete.ConcretePerson;
+import it.unisa.tirocinio.manager.concrete.ConcreteTrainingRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Valentino
+ */
+public class viewTrainingRequestForProfessorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        ConcreteMessageForServlet message = new ConcreteMessageForServlet();
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            String professorSSN = "delucia";//request.getParameter("professorAccountEmail");
+            
+            ConcretePerson aPerson = ConcretePerson.getInstance();
+            Person person = aPerson.readPersonForAccount(professorSSN);
+            
+            ConcreteTrainingRequest aTrainingRequest = ConcreteTrainingRequest.getInstance();
+            ArrayList<TrainingRequest> trainingRequest = aTrainingRequest.readTrainingRequestByProfessor(person.getSSN());
+            
+            if(trainingRequest == null){
+                message.setMessage("status", 0);
+            }else{
+                message.setMessage("status", 1);
+                message.setMessage("Object", trainingRequest);
+                for(int i = 0; i < trainingRequest.size(); i++){
+                    out.println("\n ----------------\n");
+                    out.println("id "+trainingRequest.get(i).getIdTrainingRequest());       
+                    out.println("desc "+trainingRequest.get(i).getDescription());    
+                    out.println("titl "+trainingRequest.get(i).getTitle());       
+                    out.println("org "+trainingRequest.get(i).getOrganizationVATNumber());       
+                    out.println("prof "+trainingRequest.get(i).getPersonSSN());       
+                    out.println("stud "+trainingRequest.get(i).getStudentSSN());       
+                    out.println("stat "+trainingRequest.get(i).getTrainingStatus());  
+                }
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
